0a9e35fa8340294c759ae174c86709034778aea8
unknown <harishpr546@gmail.com>
2026-02-12 10:30:46 +0530

    Config

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // src/config.ts
 export const apiUrl = "http://localhost/manager.bookpanlam/public";
-export const uploadsUrl = "http://localhost/manager.bookpanlam/public/uploads";
+export const uploadsUrl = "http://localhost/manager.bookpanlam/public/uploads/sellers";
 export const CSC_API_KEY =
   "bmhxTnNOcUhKOVlRSGNUeEdud2N0Vk9Wc09KVlRLMVBPdG5BeHNOOA==";
